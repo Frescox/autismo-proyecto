175ad3372a95978511c8f4ec9b57f8f65fbeabc8
Rafael Cabrera <rafael.cabrera32@uabc.edu.mx>
2024-10-29 21:28:20 -0700

    Cambios en manejo de base de datos

--- log-reg.php
+++ log-reg.php
@@ -12,13 +12,16 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     // Registro de usuario
     if ($form_type == 'register') {
         $user_name = $_POST['user_name'];
-        $user_name_hash = encryptData($user_name);
+        $user_name_enc = encryptData($user_name);
+
+        $user_lastname = $_POST['user_lastname'];
+        $user_lastname_enc = encryptData($user_lastname);
 
         $user_email = $_POST['user_email'];
         $user_email_hash = md5($user_email);
 
         $user_password = md5($_POST['password']);
-        $user_lastname = md5($_POST['user_lastname']);
+        
 
         $query = "SELECT user_email FROM users_sonrisas WHERE user_email = '$user_email_hash' LIMIT 1";
         $result = mysqli_query($con, $query);
@@ -29,9 +32,8 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
                 $user_date = date("y-m-d");
                 $user_id = random_num(5,$con);
-                $verify_token = md5(rand());
-                $query = "INSERT INTO users_sonrisas (user_id, user_name, user_lastname, user_email, password, date,veriftok) 
-                          VALUES ('$user_id', '$user_name_hash', '$user_lastname', '$user_email_hash', '$user_password','$user_date','$verify_token')";
+                $query = "INSERT INTO users_sonrisas (user_id, user_name, user_lastname, user_email, password, date) 
+                          VALUES ('$user_id', '$user_name_enc', '$user_lastname_enc', '$user_email_hash', '$user_password','$user_date')";
 
                 $query_run = mysqli_query($con, $query);
                 if ($query_run) {
