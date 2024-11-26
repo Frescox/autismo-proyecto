777d58a48845190928556b7b679104c96dcae8af
Jose Moreno <jose.moreno39@uabc.edu.mx>
2024-11-26 00:55:17 -0800

    Logica para eliminar cuenta del infante desde el panel de control, pendiente modificaciones visuales

--- js/verif_perm.js
+++ js/verif_perm.js
@@ -13,3 +13,35 @@ document.addEventListener("DOMContentLoaded", () => {
             window.location.href = 'log-reg.php'; // Redirige si ocurre un error
         });
 });
+
+function confirmDeleteAccount() {
+        const childUuid = localStorage.getItem("child_uuid");
+    
+        if (!childUuid) {
+            alert("No se encontró el ID del niño en el almacenamiento local.");
+            return;
+        }
+    
+        fetch("delete_account.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                child_uuid: childUuid, // Enviar el ID del niño
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    alert(data.message);
+                    window.location.href="select_acc.php"; // Opcional: recargar la página después de eliminar
+                } else {
+                    alert(data.message || "Error al eliminar los datos del niño.");
+                }
+            })
+            .catch((error) => {
+                console.error("Error al eliminar los datos del niño:", error);
+            });
+    }
+    
